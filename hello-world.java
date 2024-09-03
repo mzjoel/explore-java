96b887aa0fe92143c0f6b2043c85e1165c012094
Zulfa Aulia Hidayat <115333625+mzjoel@users.noreply.github.com>
2024-09-03 16:02:05 +0700

    initial commit

---
+++ hello-world.java
@@ -0,0 +1,18 @@
+class HelloWorld{
+    public static void main(String[] args){
+        String thisVariable = "Hallo World";
+        System.out.println(thisVariable);
+        System.out.println("Apa Kabar?");
+
+        //Blok Program IF
+        if(true){
+            System.out.println("True");
+        }
+        //Blok Program for
+        for(int i = 0; i<20; i++){
+            System.out.println("Perulangan Ke-"+i);
+        }
+        
+    }
+}
+
