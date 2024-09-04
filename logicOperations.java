50c9534ac7b466ee869f8c1b88b536761ccd5b34
Zulfa Aulia Hidayat <115333625+mzjoel@users.noreply.github.com>
2024-09-04 09:26:19 +0700

    feat:logic operator

---
+++ logicOperations.java
@@ -0,0 +1,16 @@
+public class logicOperations {
+    public static void main(String[] args){
+        boolean a = true;
+        boolean b = false;
+        boolean c;
+
+        //AND (&&)
+        c = a && b;
+        System.out.println("a && b = "+c);
+        //OR 
+        c = a || b;
+        System.out.println("a || b= "+ c);
+        //Negasi 
+        System.out.println("!a = "+!a);
+    }
+}
