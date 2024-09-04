f60af2cf15e09788ae5537f2b60d8fd277bcd35a
Zulfa Aulia Hidayat <115333625+mzjoel@users.noreply.github.com>
2024-09-04 08:18:49 +0700

    feat:learn variable & show output

---
+++ DataDiri.java
@@ -0,0 +1,20 @@
+public class DataDiri{
+    public static void main(String[] args){
+        //Inisisasi Variabel
+        String nama, alamat;
+        int usia;
+        double tinggi;
+
+        //Mengisi Variabel 
+        nama = "Mahardika Latief Surodiharjo";
+        usia = 20;
+        tinggi = 170.5;
+        alamat = "Semarang";
+
+        //Print Out 
+        System.out.println("Nama: "+nama);
+        System.out.println("Usia: "+usia);
+        System.out.println("Tinggi: "+tinggi);
+        System.out.println("Alamat: "+alamat);
+    }
+}
